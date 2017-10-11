d4165771fd8d27da19fb2792a1b659b24e1fd3ff
Yacine Achiakh <y.achiakh@criteo.com>
2017-10-10 22:11:25 -0700

    add setup, config and first function

---
+++ setup.py
@@ -0,0 +1,7 @@
+setup(
+    name='CryptoTaxCompiler',
+    version='0.1dev',
+    packages=[],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+)
