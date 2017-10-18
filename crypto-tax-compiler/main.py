8713876b8d034fcfaf2187e550538747f73843b8
Yacine Achiakh <y.achiakh@criteo.com>
2017-10-17 19:43:17 -0700

    First refacto

--- crypto-tax-compiler/main.py
+++ crypto-tax-compiler/main.py
@@ -4,6 +4,7 @@ import configparser
 import json
 import os
 import sys
+import datetime
 #sys.path.insert(0, '../config_directoy')
 import config_directory.config_file as config
 from collections import namedtuple
@@ -32,26 +33,16 @@ except:
 
 cur = conn.cursor()
 
-#To replace a given key in JSON filed
-def modify_key(obj):
-    for key in obj.keys():
-        new_key = key.replace("24h_volume_usd","volume_24h_usd")
-        if new_key != key:
-            obj[new_key] = obj[key]
-            del obj[key]
-    return obj
 
-
-# Fetching Data From API
-def api_json_fetcher(api, ticker, unwanted=[]):
-    api_json_fetched = []
-
-    for x in ticker:
-        url = urljoin(api, x)
+# Functions fetching from API
+class data_fetchers:
+    # Fetching Data From API
+    def api_json_fetcher(api, ticker, unwanted=[]):
+        url = urljoin(api, ticker)
         response = requests.get(url)
 
         # Modify json to make it tuple compliant
-        response = json.loads(response.content, object_hook=modify_key)
+        response = json.loads(response.content, object_hook=data_transformers.modify_key)
 
         # Remove useless keys
         for unwanted_key in unwanted:
@@ -59,35 +50,61 @@ def api_json_fetcher(api, ticker, unwanted=[]):
                 if unwanted_key in x:
                     del x[unwanted_key]
 
-        response=response[0]
-        api_json_fetched.append(response)
-    return api_json_fetched
-
-# Parsing Data and storing it in a tuple
-def api_json_parser(api_json_fetched):
-    results=[Daily_ticker_info(**k) for k in api_json_fetched]
-    return results
+        response = response[0]
+        return response
 
 
-a = api_json_fetcher(Coinmarketcap.DOMAIN_NAME,config.crypto_tickers_name,['percent_change_1h','percent_change_24h','percent_change_7d','price_btc'])
-print(a)
-b = api_json_parser(a)
+
+# Functions transforming fetched data
+class data_transformers:
+    #To replace a given key in JSON filed
+    def modify_key(obj):
+        for key in obj.keys():
+            new_key = key.replace("24h_volume_usd","volume_24h_usd")
+            if new_key != key:
+                obj[new_key] = obj[key]
+                del obj[key]
+        return obj
+
+
+    # Parsing Data and storing it in a tuple
+    def dict_to_daily_ticker_tuple(api_json_fetched):
+        results=[Daily_ticker_info(**k) for k in api_json_fetched]
+        return results
+
+
+
+# Functions writing in SQL
+class data_writer:
+    # Write in Postgres Table
+    def fact_price_volume_stats_daily_writer(tuple):
+        q = cur.mogrify('SELECT * FROM fact_price_volume_stats_daily WHERE id=%s and last_update=%s', (tuple[0],tuple[8]))
+        cur.execute(q)
+        rows = cur.fetchall()
+
+        #check if data has already been fetched
+        if not rows:
+            q = cur.mogrify('INSERT INTO crypto_db.public.fact_price_volume_stats_daily VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', tuple)
+            try:
+                cur.execute(q)
+            except:
+                print('Failed to insert line')
+
+
+
+# Script to Fetch the Data
+api_json_fetched = []
+for ticker in config.crypto_tickers_name:
+    a = api_json_fetcher(Coinmarketcap.DOMAIN_NAME, ticker,
+                         ['percent_change_1h', 'percent_change_24h', 'percent_change_7d', 'price_btc'])
+    print(type(a))
+    api_json_fetched.append(a)
+print(api_json_fetched)
+
+# Script to put the data into a tuple
+b = data_transformers.dict_to_daily_ticker_tuple(api_json_fetched)
 print(b)
 
-# Write in Postgres Table
-
+# Script to write the data in Postgres SQL
 for x in b:
-    print(x)
-    q = cur.mogrify('SELECT * FROM fact_price_volume_stats_daily WHERE id=%s and last_update=%s', (x[0],x[8]))
-    cur.execute(q)
-    rows = cur.fetchall()
-
-    #check if data has already been fetched
-    if not rows:
-        q = cur.mogrify('INSERT INTO crypto_db.public.fact_price_volume_stats_daily VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)', x)
-        print(q)
-
-        try:
-            cur.execute(q)
-        except:
-           print('Failed to insert line')
+    data_writer.fact_price_volume_stats_daily_writer(x)
